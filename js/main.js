c8f52c5874c9aa010755998eb0b7c59741a0a5b5
Daniel Juárez <djcekta@gmail.com>
2014-08-22 18:25:02 -0500

    Version2.0

--- js/main.js
+++ js/main.js
@@ -1,25 +1,57 @@
 
-var app = angular.module('artistApp', [])
+var app = angular.module('artistApp', []);
 
-function artistController($scope, $http) {
-	$scope.artistData = {};
+app.directive('fileChange', [
+  function() {
+    return {
+      link: function(scope, element, attrs) {
+        element[0].onchange = function() {
+          scope[attrs['fileChange']](element[0])
 
-	$http.get('http://dev.godster.mx/artist')
-		.success(function(data){
-			$scope.artists = data;
-		})
-		.error(function(data){
-			console.log('Error: ' + data);
-		});
+        }
+      }
 
-	$scope.addArtist = function() {
-		console.log($scope.artistData);
-		$http.post('http://godster.mx:5000/artist', $scope.artistData)	
-			.success(function(data){
-				$scope.artistData = {};
-			})
-			.error(function(data){
-				console.log('Error: ' + data);
-			});
-	}
-};
+    }
+  }
+  ])
+
+app.controller('artistController', function($scope, $http){
+
+  $http.get('http://dev.godster.mx/artist')
+  .success(function(data){
+    $scope.artists = data.response;
+  })
+  .error(function(data){
+    console.log('Error: ' + data);
+  });
+
+  $scope.addArtist = function() {
+    var res = encodeURIComponent('background_image=' + $scope.background_image[0].name + '&profile_image=' + $scope.profile_image[0].name + '&name=' + $scope.name + '&genre=' + $scope.genre);
+    $http({
+      method: "post",
+      url: " http://dev.godster.mx/artist",
+      headers: {
+        "Content-type":"application/x-www-form-urlencoded; charset=utf-8"
+      },
+      data: res
+    })
+    .success(function(data){
+      console.log(data);
+    })
+    .error(function(data){
+      console.log('Error: ' + data);
+    });
+  }
+
+  $scope.backgroundFile = function(elm){
+    $scope.background_image=elm.files;
+    console.log($scope.background_image[0].name);
+    $scope.$apply();
+  }
+
+  $scope.profileFile = function(elm){
+    $scope.profile_image=elm.files;
+    console.log($scope.profile_image[0].name);
+    $scope.$apply();
+  }
+});
